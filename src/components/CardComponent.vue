b64d0b7f003fb6926ed4f24029fb63a07b1298c6
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-05-03 22:26:51 +0500

    Tovar card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,6 +1,9 @@
 <template>
   <q-card class="my-card">
     <img src="https://loremflickr.com/640/560" class="card-img">
+    <q-card-section>
+      <div class="my-card-text text-h6 q-pb-none">{{ nameTrimmedText }}</div>
+    </q-card-section>
 
     <q-card-section>
       <div class="my-card-text text-h6 q-pb-none">1.999₽</div>
@@ -19,6 +22,16 @@
 <script setup>
   let lorem ='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   if (lorem.length > 100) lorem = lorem.slice(0, 200) + '...';
+  
+  const trimSizeforItemName = 20;
+  let nameText = 'TovarName long Something that is too big and you want to trim, and then renddeer in your HTML page.';
+  let nameContent = nameText.substring(0, nameText.length - 4);
+  let nameTrimmedText = nameContent.substring(0, trimSizeforItemName) + '...';
+
+  // const trimSizeforItemDescr = 20;
+  // let text = 'Something that is too big and you want to trim, and then renddeer in your HTML page.';
+  // let content = text.substring(0, text.length - 4);
+  // let trimmedText = content.substring(0, trimSizeforItemDescr) + '...';
 </script>
 
 <style lang="sass" scoped>
