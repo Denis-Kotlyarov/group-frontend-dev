ab9376bcd05ab2bcc0d79811f8fc4e6541f3c8d9
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-05-03 18:31:43 +0300

    Add firebase and fav. icon to the card component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -10,15 +10,24 @@
       {{ lorem }}
     </q-card-section>
 
-    <q-card-section class="q-pt-none q-w-100">
-      <q-btn class="full-width" color="primary" label="Купить" />
+    <q-card-section class="q-pt-none q-w-100 row q-gutter-x-sm">
+      <q-btn class="col-9" color="primary" label="Купить" />
+      <q-icon 
+        name="favorite" 
+        size="md"
+        class="favorite-icon cursor-pointer col-2" 
+        :class="favoriteToggler ? 'text-red-10' : 'text-grey-7'" 
+        @click="favoriteToggler = !favoriteToggler"/>
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
+  import { ref } from "vue"
   let lorem ='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   if (lorem.length > 100) lorem = lorem.slice(0, 200) + '...';
+
+  let favoriteToggler = ref(false)
 </script>
 
 <style lang="sass" scoped>
@@ -29,4 +38,7 @@
   .card-img
     max-width: 280px
     max-height: 300px
+  
+  .favorite-icon
+    transition: color 0.3s ease-in-out
 </style>
