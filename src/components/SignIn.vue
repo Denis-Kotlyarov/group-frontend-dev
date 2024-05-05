10f85c54d7cb018e41fe44828b1bbf792ddafec2
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-05-05 13:46:56 +0500

    Переименовал функцию

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -4,7 +4,7 @@
   <p><input type="text" placeholder="email" v-model="email"/></p>
   <p><input type="password" placeholder="password" v-model="password"/></p>
   <p v-if="errMsg">{{ errMsg }}</p>
-  <p><button @click="register">Submit</button></p>
+  <p><button @click="signin">Submit</button></p>
   <p><button @click="singInWithGoogle">singInWithGoogle</button></p>
 </template>
 
@@ -18,7 +18,7 @@ const password = ref("");
 const errMsg = ref(""); //ERROR message
 const router = useRouter();
 
-const register = () => {
+const signin = () => {
   signInWithEmailAndPassword(getAuth(), email.value, password.value)
   .then((data) =>{
     console.log("Signed you!");
